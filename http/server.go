64a3d4bce24841a69fcb85e880e2892fe7ccb42a
Máximo Cuadros Ortiz <mcuadros@gmail.com>
2014-10-16 22:44:58 +0200

    frontend tests

--- http/server.go
+++ http/server.go
@@ -2,10 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 var router = gin.Default()
 
 func main() {
+	router.GET("/", func(c *gin.Context) {
+		http.ServeFile(c.Writer, c.Request, "static/index.html")
+	})
+
 	router.Run(":8080")
 }
